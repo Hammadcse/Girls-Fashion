08b9752b1f5bf94cbc47c0ff831ff9f83062fc98
Md Hammadur Rahman <73161673+Hammadcse@users.noreply.github.com>
2022-08-13 02:24:03 +0600

    home page design

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+// services slider
+$('.service_carousel').owlCarousel({
+  loop:true,
+  dots:false,
+  margin:16,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      },
+      1000:{
+          items:3
+      }
+  }
+})
+
+
+
+
